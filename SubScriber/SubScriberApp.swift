a51a7576a5d36166d9c44670173edcd15a12019c
syi07030 <syii07030@gmail.com>
2022-01-25 22:55:26 +0900

    2022.1.25[1]

---
+++ SubScriber/SubScriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubScriberApp.swift
+//  SubScriber
+//
+//  Created by HanGeunyoung on 2022/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SubScriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
